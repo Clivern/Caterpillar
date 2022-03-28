7d293f10a16ddad12016e1fcd1ea41bc0e5363f3
Clivern <hello@clivern.com>
2022-03-28 23:38:39 +0200

    init

---
+++ app/Exceptions/ResourceNotFoundException.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
+
+/**
+ * Class ResourceNotFoundException.
+ */
+class ResourceNotFoundException extends Exception
+{
+    /**
+     * Class Constructor.
+     *
+     * @param string    $message
+     * @param int       $code
+     * @param Exception $previous
+     */
+    public function __construct(
+        string $message,
+        $code = 0,
+        Exception $previous = null
+    ) {
+        parent::__construct($message, $code, $previous);
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return sprintf(
+            'Exception \'%s\' triggered %s%s',
+            static::class,
+            \PHP_EOL,
+            parent::__toString()
+        );
+    }
+
+    /**
+     * Report the exception.
+     *
+     * @return void
+     */
+    public function report()
+    {
+        Log::debug(sprintf("ResourceNotFound Exception raised: %s", $this->getMessage()));
+    }
+
+    /**
+     * Render the exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function render($request)
+    {
+        return response()->json([
+            'errorMessage' => $this->getMessage(),
+            'correlationId' => $request->headers->get('X-Correlation-ID'),
+        ], Response::HTTP_NOT_FOUND);
+    }
+}
